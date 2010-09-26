ca5a259b1112cb8b91b43d46f97e484a2d9dc837
xydudu <xuedudu@gmail.com>
2010-09-26 10:44:12 +0800

    im demo

--- extend/im.source.js
+++ extend/im.source.js
@@ -11,13 +11,17 @@ window.HN && window.APE && (HN.IM = function($fun) {
     
     var 
     client = new APE.Client(),
+    ape,
     pipe = 0;
     
     client.load({
         identifier: 'honey-im',
         transport: 2,
         channel: 'test',
-        complete: $fun 
+        complete: function($ape) {
+            ape = $ape; 
+            $fun && $fun($ape);
+        } 
     });
 
     //出错
@@ -28,31 +32,44 @@ window.HN && window.APE && (HN.IM = function($fun) {
 
     //连接返回
     client.onRaw('login', function($data) {
-        HN.debug('login')
-        HN.debug($data)
+        HN.debug('login');
+        HN.debug($data);
     });
     
     //监听请求聊天    
     client.onRaw('SLT_INVITE', function($data) {
         HN.debug('SLT_INVITE');
         HN.debug($data);
+        var 
+        //获取向你发送聊天邀请的人
+        uin = $data.data.user.properties.uin,
+        //是否接受？ 
+        ok = confirm(uin +' want to chat with you, chat or not?');
+        
+        ape.request.send(
+            'SLT_RSP', 
+            {'uid': uin, 'op': ok ? 1 : 3}
+        );
+    });
+
+    //监听确定请求
+    client.onRaw('SLT_RESPONSE', function($data) {
+        HN.debug('SLT_RESPONSE');     
+        
     });
   
     return {
         
         //连接
         connect: function($ape, $uin) {
-
             $ape.start({"uin": $uin}); 
-            
         },
         
         //请求聊天
         requestChat: function($ape, $uin) {
-
             $ape.request.send('SLT_REQ', {"uid": $uin}); 
 
         }
-        
+
     };
 });

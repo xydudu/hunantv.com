1ac581d79bbadd3ddc25a58f91fc82dcd73060b0
xydudu <xuedudu@gmail.com>
2010-09-27 11:18:26 +0800

    im test

--- extend/im.source.js
+++ extend/im.source.js
@@ -30,10 +30,22 @@ window.HN && window.APE && (HN.IM = function($fun) {
         HN.debug($data);        
     });
 
-    //连接返回
-    client.onRaw('login', function($data) {
+    //登陆成功
+    client.onRaw('LOGIN', function($data) {
+
         HN.debug('login');
         HN.debug($data);
+        $('#request-chat').show();
+        $('#status').html('you are online()');
+
+    });
+    //已连接
+    client.onRaw('IDENT', function($data) {
+        
+        HN.debug('ident');
+        $('#request-chat').show();
+        $('#status').html('you are online('+ $data.data.user.properties.uin +')');
+
     });
     
     //监听请求聊天    
@@ -50,14 +62,39 @@ window.HN && window.APE && (HN.IM = function($fun) {
             'SLT_RSP', 
             {'uid': uin, 'op': ok ? 1 : 3}
         );
+        
+        ok && goChat(uin);
+
+    });
+
+    //可以直接聊天的状态
+    client.onRaw('SLT_REQ', function($data) {
+        
+        $('#request-chat').hide();
+        $('#send-msg').show();
+        $('#status').html('you can chat with ('+ $data.data.user.properties.uin +') now!');
+
     });
 
     //监听确定请求
     client.onRaw('SLT_RESPONSE', function($data) {
         HN.debug('SLT_RESPONSE');     
-        
+        HN.debug($data);     
+        var o = $data.data;
+        if (+o.op === 1) {
+            //接受了
+            HN.debug(o.user.properties.uin +'接受了跟你聊天。。');
+            goChat(o.user.properties.uin);
+        } else {
+            HN.debug(o.op); 
+        }
     });
   
+    function goChat($uin) {
+        HN.debug($uin +'与你聊天中。。');
+        $('#request-chat').hide();
+        $('#send-msg').show();
+    }
     return {
         
         //连接
@@ -68,7 +105,11 @@ window.HN && window.APE && (HN.IM = function($fun) {
         //请求聊天
         requestChat: function($ape, $uin) {
             $ape.request.send('SLT_REQ', {"uid": $uin}); 
+        },
 
+        //断开聊天
+        closeChat: function($ape, $uin) {
+            $ape.request.send('SLT_LEFT', {"uid": $uin}); 
         }
 
     };
